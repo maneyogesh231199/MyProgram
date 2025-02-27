95240f96c2c2971553b6d098e7dd8ac5d861a6f2
maneyogesh231199 <maneyogesh804@gmail.com>
2025-02-27 13:48:10 +0530

    Add files via upload

---
+++ Program32.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay SwamiSamrthe...\n");
+    }
+
+}
+int main()
+
+{
+    Display(8);
+    
+    return 0;
+}
